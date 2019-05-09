5a25dbe697c6e66e66305f5304e02bfecedf5b39
codeFanDot <>
2019-05-09 13:58:17 -0500

    Added page template for Index

--- LearningDjangoWeb/HelloDjangoApp/views.py
+++ LearningDjangoWeb/HelloDjangoApp/views.py
@@ -3,9 +3,18 @@ from django.http import HttpResponse
 from datetime import datetime
 
 # Create your views here.
+
 def index(request):
-    now = datetime.now()
-    html_content = "<html><head><title>Hello, Django</title></head><body>"
-    html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
-    html_content += "</body></html>"
-    return HttpResponse(html_content)
+     now = datetime.now()
+
+     return render(
+        request,
+        "HelloDjangoApp/index.html",  # Relative path from the 'templates' folder to the template file
+        # "index.html", # Use this code for VS 2017 15.7 and earlier
+        {
+            'title' : "Hello Django",
+            'message' : "Hello Django!",
+            'content' : " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
+          
